79e9e245cda3b6f0b108679284c8a5f7c4ae8fcd
nkomof <ffnkomo@gmail.com>
2019-07-15 14:52:13 +0200

    Add files via upload
    Added a few test cases. More is coming.

--- springboot_swagger_demo/src/test/java/com/totalbeginner/springbootswaggerdemo/resource/UserResourceTest.java
+++ springboot_swagger_demo/src/test/java/com/totalbeginner/springbootswaggerdemo/resource/UserResourceTest.java
@@ -1,35 +1,67 @@
 package com.totalbeginner.springbootswaggerdemo.resource;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.when;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.json.JSONException;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
 
-import com.totalbeginner.springbootswaggerdemo.repository.UserRepository;
+import io.restassured.RestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
 
 
 public class UserResourceTest {
 	
-	@InjectMocks
-	UserResource userResource;
-	@Mock
-	UserRepository userRepository;
-	
-	@BeforeEach
-	void setUp() throws Exception {
-		MockitoAnnotations.initMocks(this);
+	private static ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder();
+
+	@BeforeClass
+	public static void init() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = 8088;
+		responseSpecBuilder.expectStatusCode(200);
+		responseSpecBuilder.expectContentType(MediaType.APPLICATION_JSON_VALUE);
 	}
 	
 	@Test
-	final void testGetUserName() {
-		Integer currentId = 120;
-		User user = User.builder().id(++currentId).userName("Luke").salary(300L).build();		
-		when(userRepository.findByUserName(anyString())).thenReturn(user);
-		assertEquals("Luke", user.getUserName());
-	}	
+	public void testGetAUserById(){
+		given()
+		.when()
+		.get("/rest/users/id/{id}", 121)
+		.then()
+			.log()
+			.ifValidationFails()
+			.spec(responseSpecBuilder.build())
+			.body("id", equalTo(121));
+	}
+	
+	@Test
+	public void testGetAll() {
+		given()
+		.when()
+			.get("/rest/users/all")
+		.then()
+			.log()
+			.ifValidationFails()
+			.spec(responseSpecBuilder.build())
+		.body("userName", hasItems("Sylvester"));
+	}
+	
+	@Test
+	public void testCreateUser() throws JSONException {
+		User user = new User();
+		user.setUserName("Jonas");
+		user.setSalary(50l);
+		user.setId(129);
+		
+		given()
+			.body(user)
+			.header("Content-Type", "application/json")
+			.when()
+			.post("/rest/users/adduser")
+			.then()
+			.spec(responseSpecBuilder.build());
+	}
 }
